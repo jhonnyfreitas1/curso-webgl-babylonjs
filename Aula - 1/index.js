4a15c4fab8bb19e4b248ce4fd76484fe6a96b95a
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2020-04-05 20:13:30 -0300

    Aula 2 and aula 3

--- Aula - 1/index.js
+++ Aula - 1/index.js
@@ -1,35 +1,35 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded'; () => {
 
     const canvas = document.getElementById('renderCanvas');
 
-    const engine = new BABYLON.Engine(canvas, true);
+    const engine = new BABYLON.Engine(canvas; true);
 
     const scene = new BABYLON.Scene(engine);
 
-    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0,0,-30), scene);
+    const camera = new BABYLON.FreeCamera('camera'; new BABYLON.Vector3(0;0;-30); scene);
 
-    const light = new BABYLON.HemisphericLight('light',  new BABYLON.Vector3(0,5,0),scene);
+    const light = new BABYLON.HemisphericLight('light';  new BABYLON.Vector3(0;5;0);scene);
     
-    const sphere = BABYLON.Mesh.CreateSphere('bola', 20,2,scene);
+    const sphere = BABYLON.Mesh.CreateSphere('bola'; 20;2;scene);
 
-    const box = BABYLON.Mesh.CreateBox('box', 3 ,scene);
+    const box = BABYLON.Mesh.CreateBox('box'; 3 ;scene);
 
     box.position.x = -5;
 
-    scene.clearColor = new BABYLON.Color3(0.23,0.162,0.184);
+    scene.clearColor = new BABYLON.Color3(0.23;0.162;0.184);
 
     camera.attachControl(canvas);
 
-    const skybox = BABYLON.Mesh.CreateBox("skyBox", 100.0, scene);
+    const skybox = BABYLON.Mesh.CreateBox("skyBox"; 100.0; scene);
     
     const skyboxMaterial = new BABYLON.StandardMaterial(
-      "skyBox", scene);
+      "skyBox"; scene);
 
 
     skyboxMaterial.backFaceCulling = false;
     
     skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture(
-      "https://cdn.glitch.com/a9d62d9b-a5bd-494d-b475-c4dd9173effa%2Fgalaxy.png?v=1582423682923",
+      "https://cdn.glitch.com/a9d62d9b-a5bd-494d-b475-c4dd9173effa%2Fgalaxy.png?v=1582423682923";
       scene
     );
     skyboxMaterial.disableLighting = true;
@@ -40,20 +40,20 @@ window.addEventListener('DOMContentLoaded', () => {
     setTimeout(()=>{
 
         caixas2 = [
-            BABYLON.Mesh.CreateBox("skyBox", 2.3, scene),
-            BABYLON.Mesh.CreateBox("skyBox", 5, scene),
-            BABYLON.Mesh.CreateBox("skyBox", 1, scene),
-            BABYLON.Mesh.CreateBox("skyBox", 8, scene),
-            BABYLON.Mesh.CreateBox("skyBox", 3, scene),
-            BABYLON.Mesh.CreateBox("skyBox", 2.5, scene)
+            BABYLON.Mesh.CreateBox("skyBox"; 2.3; scene);
+            BABYLON.Mesh.CreateBox("skyBox"; 5; scene);
+            BABYLON.Mesh.CreateBox("skyBox"; 1; scene);
+            BABYLON.Mesh.CreateBox("skyBox"; 8; scene);
+            BABYLON.Mesh.CreateBox("skyBox"; 3; scene);
+            BABYLON.Mesh.CreateBox("skyBox"; 2.5; scene)
         ];
 
 
 
-        caixas.map((item,index) => 
+        caixas.map((item;index) => 
             item.position.x = Math.random
         );
-    },2000)
+    };2000)
 
     engine.runRenderLoop(()=>{
 
--- Aula - 2/app.js
+++ Aula - 2/app.js
@@ -5,59 +5,21 @@ window.addEventListener('DOMContentLoaded', () => {
     const engine = new BABYLON.Engine(canvas, true);
     ///OBJETO DE CENA
     const scene = new BABYLON.Scene(engine);
-    //CAMERA QUE NOS PERMITE OBSERVAR A CENA
-    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0,0,-30), scene);
-    //LUZ 
-    const light = new BABYLON.HemisphericLight('light',  new BABYLON.Vector3(0,5,0),scene);
-    //CAIXA
-    const box = new BABYLON.Mesh.CreateBox('box', 2,scene);
-    //CHAO 
-    const ground = new BABYLON.Mesh.CreateGround('CHAO',10,10,1,scene);
-    ground.position.y = -2;
-
-    //torus/
-    const torus = new BABYLON.Mesh.CreateTorus('torus',4,0.8,20, scene);
-    torus.position.y = 4;
-
-    //cylinder
-    const  cylinder = new BABYLON.Mesh.CreateCylinder('cilindro', 3.5,1,2.5,20,1,scene);
-    cylinder.position.z = -3;
-    cylinder.position.x = -3;
-
-    //plano
-    const  plane = new BABYLON.Mesh.CreatePlane('plane', 2,scene );
-    plane.position.x =3;
 
 
-    const Linha1 = new BABYLON.Mesh.CreateLines('linhas1', [
-        new BABYLON.Vector3(0,0,0),
-        new BABYLON.Vector3(0,5,0),
-        new BABYLON.Vector3(-3,5,0),
-        new BABYLON.Vector3(3,5,0),
-    ],scene);
+    const camera = new BABYLON.UniversalCamera('camera',new BABYLON.Vector3(0,0,-15),scene );
 
-    
-    const J = new BABYLON.Mesh.CreateLines('J', [
-        new BABYLON.Vector3(0,0,0),
-        new BABYLON.Vector3(-1,1,0),
-        new BABYLON.Vector3(0,0,0),
-        new BABYLON.Vector3(0,3,0),
-        new BABYLON.Vector3(-1,3,0),
-        new BABYLON.Vector3(1,3,0),
-    ],scene);
-
-    J.position.x = 6;
-    camera.position.y =3;
-    camera.position.z =-13;
     camera.attachControl(canvas);
-
-    engine.runRenderLoop(()=>{
-        
-       scene.render();
-
-    });
-
-
-
     
+    const light = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(0,5,0),scene);
+
+    const sphere  = BABYLON.Mesh.CreateSphere('sphere', 10, 5, scene);
+    
+    const sphere2  = BABYLON.Mesh.CreateSphere('sphere', 10, 5, scene);
+
+    sphere2.position.x = -15;
+
+   engine.runRenderLoop(()=>{
+       scene.render();  
+    }); 
 });
